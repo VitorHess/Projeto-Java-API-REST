335464538ff3f2a2bb6b803d9696ae730d038382
Joao Vitor Hess Nardes <jvhn2006@gmail.com>
2024-02-16 19:04:46 -0300

    cria repository e config de application

--- src/main/java/com/portfolio/apirest/repository/UserRepository.java
+++ src/main/java/com/portfolio/apirest/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package com.portfolio.apirest.repository;
 
-public class Repository {
-    
+import com.portfolio.apirest.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>
+{
 }
