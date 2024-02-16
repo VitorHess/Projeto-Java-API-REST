2592d01c0a4c714d19801ef8efa688a3e7b71876
Joao Vitor Hess Nardes <jvhn2006@gmail.com>
2024-02-16 18:51:11 -0300

    cria entity User

---
+++ src/main/java/com/portfolio/apirest/entity/User.java
@@ -0,0 +1,40 @@
+package com.portfolio.apirest.entity;
+import jakarta.persistence.*;
+
+@Entity
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/portfolio/apirest/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.portfolio.apirest.repository;
+
+public class Repository {
+    
+}
